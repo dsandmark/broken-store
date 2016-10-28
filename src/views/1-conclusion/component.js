cd580e705625c3a3dba90569a7535f5e06bbcc17
Dennis Sandmark <d.sandmark@gmail.com>
2016-10-28 14:46:34 +0100

    Adding conclusion

--- src/views/1-conclusion/component.js
+++ src/views/1-conclusion/component.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import keydown, { Keys } from 'react-keydown'
+import '../common.css'
 
 class Conclusion extends Component {
   componentWillReceiveProps({keydown}) {
     if (keydown.event) {
       switch (keydown.event.which) {
+        case Keys.left:
+          this.props.router.push('/')
+          break
         case Keys.right:
           this.props.router.push('first-overview')
           break
@@ -16,7 +20,15 @@ class Conclusion extends Component {
 
   render() {
     return (
-      <p>Conclusion</p>
+      <div className='slide-container'>
+        <h1 className='slide-title'>
+          Conclusion
+        </h1>
+
+        <h2 className='slide-paragraph'>
+          You should use React & Redux for web development
+        </h2>
+      </div>
     )
   }
 }
