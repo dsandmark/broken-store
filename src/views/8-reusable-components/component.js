6fd3d09c4a69d494e4758dc0add6c975bf62f574
Dennis Sandmark <d.sandmark@gmail.com>
2016-10-30 15:06:19 +0000

    Adding components slide

--- src/views/8-reusable-components/component.js
+++ src/views/8-reusable-components/component.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
 import keydown, { Keys } from 'react-keydown'
+import Modal from 'react-modal'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import moment from 'moment'
+
+const modalButtonStyle = {
+  marginRight: 20,
+}
+
+const modalCloseButtonStyle = {
+  position: 'absolute',
+  bottom: 20,
+  right: 20,
+}
 
 class ReusableComponents extends Component {
+  constructor() {
+    super()
+    this.state = {
+      modalIsOpen: false,
+      startDate: moment(),
+    }
+  }
+
   componentWillReceiveProps({keydown}) {
     if (keydown.event) {
       switch (keydown.event.which) {
@@ -17,9 +46,62 @@ class ReusableComponents extends Component {
     }
   }
 
+  openModal = () => {
+    this.setState({
+      modalIsOpen: true,
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
+      modalIsOpen: false,
+    })
+  }
+
+  handleChange = (date) => {
+    this.setState({
+      startDate: date,
+    })
+  }
+
   render() {
     return (
-      <p>ReusableComponents</p>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div className='slide-container'>
+          <h1 className='slide-title'>
+            Reusable Components
+          </h1>
+
+          <h2 className='slide-subtitle'>No need to reinvent the wheel</h2>
+
+          <RaisedButton
+            onClick={this.openModal}
+            label='Modal'
+            primary={true}
+            style={modalButtonStyle}
+          />
+
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+          />
+
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            contentLabel="Modal Example"
+          >
+            <h1>Modal Content</h1>
+            <p>Lorem ipsum...</p>
+
+            <FlatButton
+              onClick={this.closeModal}
+              label='Close'
+              primary={true}
+              style={modalCloseButtonStyle}
+            />
+          </Modal>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
